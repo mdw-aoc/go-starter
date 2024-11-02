aba30dbad1a2f8922525c3f69c643fb92ddc3317
Michael Whatcott <mdwhatcott@users.noreply.github.com>
2024-11-01 23:06:20 -0600

    Initial commit.

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/mdw-aoc/go-starter
+
+go 1.23
+
+require (
+	github.com/mdw-aoc/inputs/v2 v2.1.0
+	github.com/mdw-go/funcy v1.13.0
+	github.com/mdw-go/must v1.4.0
+	github.com/mdw-go/set/v2 v2.4.0
+	github.com/mdw-go/testing v1.5.0
+)
---
+++ main_test.go
@@ -0,0 +1,53 @@
+package starter
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+
+	"github.com/mdw-aoc/inputs/v2/inputs"
+	_ "github.com/mdw-go/funcy"
+	_ "github.com/mdw-go/set/v2/set"
+	_ "github.com/mdw-go/must/must"
+	"github.com/mdw-go/testing/should"
+)
+
+const TODO = -1
+
+var (
+	inputLines  = slices.Collect(inputs.Read(TODO, TODO).Lines())
+	sampleLines = []string{
+		fmt.Sprint(TODO),
+	}
+)
+
+func TestSuite(t *testing.T) {
+	should.Run(&Suite{T: should.New(t)}, should.Options.UnitTests())
+}
+
+type Suite struct {
+	*should.T
+}
+
+func (this *Suite) Setup() {
+}
+
+func (this *Suite) TestPart1A() {
+	this.So(this.Part1(sampleLines), should.Equal, TODO)
+}
+func (this *Suite) TestPart1Full() {
+	this.So(this.Part1(inputLines), should.Equal, TODO)
+}
+func (this *Suite) TestPart2A() {
+	this.So(this.Part2(sampleLines), should.Equal, TODO)
+}
+func (this *Suite) TestPart2Full() {
+	this.So(this.Part2(inputLines), should.Equal, TODO)
+}
+func (this *Suite) Part1(lines []string) any {
+	return TODO
+}
+
+func (this *Suite) Part2(lines []string) any {
+	return TODO
+}
